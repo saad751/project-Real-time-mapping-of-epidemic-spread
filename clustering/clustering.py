ab3cd27c66b29bec76307fc03b18dab25928e2a8
SADIYA MOHAMMEDI <145110172+saad751@users.noreply.github.com>
2025-01-20 15:03:59 +0530

    Update and rename clustering.ipynb to clustering.py

---
+++ clustering/clustering.py
@@ -0,0 +1,15 @@
+import pickle
+from flask import jsonify
+# with open("final_list.txt", "rb") as fp:
+#     list_final = pickle.load(fp)
+
+def ret_cluster_list(name):
+    with open("final_list.txt", "rb") as fp:
+        list_final = pickle.load(fp)
+    for i in list_final:
+        if name in i:
+            print(type(i))
+            return i
+print(ret_cluster_list('Goa'))
+li = jsonify(ret_cluster_list('Orissa'))
+ret_cluster_list('Meghalaya')
